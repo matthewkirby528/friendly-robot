91ac8c5c34cc0d5f33bfbd5c69d62b7cf2298005
matthewkirby528 <mkirby528@gmail.com>
2016-10-01 12:19:54 -0400

    Added work

--- src/prob7.java
+++ src/prob7.java
@@ -1,7 +1,27 @@
-package PACKAGE_NAME;
-
 /**
  * Created by mkirby528 on 10/1/2016.
  */
 public class prob7 {
+    public static void main(String[] args) {
+        int count = 0;
+        for (int x = 0; x <=1000000000; x++){
+            if (isPrime(x)){
+                count++;
+                if (count == 10001) {
+                    System.out.println(x);
+                    System.exit(0);
+                }
+        }
+
+        }
+    }
+    private static boolean isPrime(int num) {
+        if (num < 2) return false;
+        if (num == 2) return true;
+        if (num % 2 == 0) return false;
+        for (int i = 3; i * i <= num; i += 2)
+            if (num % i == 0) return false;
+        return true;
+    }
+
 }
